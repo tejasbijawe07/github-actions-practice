d204fca9b431530749068774cb3f89cb98f36c4e
tejasbijawe07 <tejasbijawe07@gmail.com>
2026-08-14 03:28:14 +0000

    Scripts and log

--- scripts/log_analyzer.sh
+++ scripts/log_analyzer.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-#Script name: log_analyzer.sh
+# Script Name: log_analyzer.sh
 
-#Check if log file path argument is provided
-
+# Check if log file path argument is provided
 if [ $# -eq 0 ]; then
     echo "Error: No log file provided."
     echo "Usage: $0 <log_file_path>"
@@ -13,10 +12,10 @@ fi
 # Store argument in variable
 LOG_FILE="$1"
 
-#Check if file exists
-if [ ! -f  "$LOG_FILE" ]; then
-   echo "Error: File '$LOG_FILE' does not exist."
-   exit 1
+# Check if file exists
+if [ ! -f "$LOG_FILE" ]; then
+    echo "Error: File '$LOG_FILE' does not exist."
+    exit 1
 fi
 
 echo "Log file '$LOG_FILE' found successfully."
